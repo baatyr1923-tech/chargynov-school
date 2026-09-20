ba0c2d57558950ac6c4cad5db987a01fb5f41fae
baatyr1923-tech <baatyr1923@gmail.com>
2026-09-20 10:09:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const menu=document.querySelector('.menu');const nav=document.querySelector('nav');menu.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
+const menu=document.querySelector('.menu');const nav=document.querySelector('nav');menu.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));const botButton = document.getElementById("bot-button");
+const botBox = document.getElementById("bot-box");
+const botClose = document.getElementById("bot-close");
+const botQuestions = document.querySelectorAll(".bot-question");
+const botAnswer = document.getElementById("bot-answer");
+
+if (botButton && botBox) {
+  botButton.addEventListener("click", function () {
+    botBox.style.display = "block";
+    botButton.style.display = "none";
+  });
+}
+
+if (botClose && botBox && botButton) {
+  botClose.addEventListener("click", function () {
+    botBox.style.display = "none";
+    botButton.style.display = "block";
+  });
+}
+
+botQuestions.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const answer = this.getAttribute("data-answer");
+
+    botAnswer.innerHTML =
+      "<strong>Бот:</strong> " + answer;
+  });
+});
